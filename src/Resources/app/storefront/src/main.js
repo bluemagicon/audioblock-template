8c74b59bf4fc16faefbee418d7f4eb52dc9da0d8
Dennis Deux <dennis@deux.de>
2020-05-28 17:25:35 +0200

    Test123

--- src/Resources/app/storefront/src/main.js
+++
@@ -1,3 +0,0 @@
-import Rumble from './script/rumble'
-
-window.PluginManager.register('Rumble', Rumble, '.btn-buy')
--- src/Resources/app/storefront/src/script/rumble.js
+++
@@ -1,7 +0,0 @@
-import Plugin from 'src/plugin-system/plugin.class'
-
-export default class Rumble extends Plugin {
-    init(){
-        console.info(window.PluginManager.getPluginInstancesFromElement(this.el))
-    }
-}
